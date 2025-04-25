1e0ca3f70b1a38bea7269b717eafb455e3c41345
MatheusMcod <matheus.moreiracosta300@gmail.com>
2025-04-25 15:38:43 -0300

    add-created-at

--- users/models.py
+++ users/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    username = None
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    username = None
+    created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
